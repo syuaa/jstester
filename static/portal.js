0f69734c5d6f531706ff57591c755fd3f7302683
syuaa <syuaa@mayapada>
2013-01-23 06:14:52 +0700

    - Resize editor to fit 80 chars. - Add short-cut button to the run button, and tab selection. - Add save LocalStorage fitur to save the work for future use.

--- static/portal.js
+++ static/portal.js
@@ -19,6 +19,8 @@ $( document ).ready( function() {
                 
                 $( ".panel" ).css( "display", "none" );
                 $( "#panel-" + txt ).css( "display", "block" );
+                var eidid = $( tgr ).attr( "data-editor" );
+                _editor[ eidid ].focus();
             } );
         }
     } );
@@ -29,10 +31,41 @@ $( document ).ready( function() {
             editor = CodeMirror.fromTextArea( e, {mode: "text/" + mime, lineNumbers:true} ),
             eidid = String( $( e ).attr( "id" ) ).replace( "edit-", "" );
             _editor[ eidid ] = editor;
+            if( typeof localStorage !== "undefined" && typeof localStorage[ eidid ] !== "undefined" )
+                editor.setValue( localStorage[ eidid ] );
     } );
     
     $( ".CodeMirror" ).height( ch - eh );
     $( "#editor-menu > li:nth-child(2)" ).click();
+    
+    $( window ).keydown( function( e ) {
+        var keyCode = String.fromCharCode( e.which ).toLowerCase();
+        if( e.ctrlKey ) {
+            if( keyCode == "s" ) {
+                e.preventDefault();
+                viewIt();
+                return false;
+                
+            }else if( e.shiftKey && ( e.which == 34 || e.which == 33 ) ){
+                var index = 1, max = 0;
+                $( "#editor-menu > li" ).each( function( i, e ) {
+                    if( $( e ).hasClass( "selected" ) )
+                        index = i;
+                    max = i;
+                } );
+                if( e.which == 33 )
+                    index--;
+                else
+                    index++;
+                
+                if( index == 0 )
+                    index = max;
+                else if ( index > max )
+                    index = 1;
+                $( "#editor-menu > li:nth-child(" + ( index+1 ) + ")" ).click();
+            }
+        }
+    } );
 } );
 
 function viewIt() {
@@ -53,4 +86,9 @@ function viewIt() {
     ifrm.document.open();
     ifrm.document.write( tx );
     ifrm.document.close();
+    
+    if( typeof localStorage !== "undefined" ) {
+        for( var k in _editor )
+            localStorage[k] = _editor[k].getValue();
+    }
 }
